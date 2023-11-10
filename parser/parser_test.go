faed019b0faefc8efe6c24cb5f3b90993208c849
LronDC <lun.su@daocloud.io>
2023-11-10 11:44:46 +0800

    Feat: add parser tool to replace template yaml

---
+++ parser/parser_test.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestReplaceTemplate(t *testing.T) {
+	// 测试用例1：正常替换
+	yamlTemplate := "apiVersion: v1\nkind: Pod\nmetadata:\n  name: {{ .Name }}\nspec:\n  containers:\n  - name: my-container\n    image: {{ .Image }}"
+	data := map[string]interface{}{
+		"Name":  "my-pod",
+		"Image": "nginx:latest",
+	}
+	expectedResult := "apiVersion: v1\nkind: Pod\nmetadata:\n  name: my-pod\nspec:\n  containers:\n  - name: my-container\n    image: nginx:latest"
+
+	result, err := replaceTemplate(yamlTemplate, data)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if result != expectedResult {
+		t.Errorf("Expected:\n%s\n\nGot:\n%s", expectedResult, result)
+	}
+
+	// 测试用例2：模板错误
+	invalidYAML := "apiVersion: v1\nkind: Pod\nmetadata:\n  name: {{ .Name\nspec:\n  containers:\n  - name: my-container\n    image: {{ .Image }}"
+	_, err = replaceTemplate(invalidYAML, data)
+	if err == nil {
+		t.Error("Expected an error for invalid YAML template, but got none.")
+	}
+}
