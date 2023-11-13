8c3531505f9f691b5f23674a1030f633f66556ae
LronDC <lun.su@daocloud.io>
2023-11-13 12:50:06 +0800

    Feat: add parser tool to replace template yaml

--- parser/parser.go
+++ parser/parser.go
@@ -5,7 +5,7 @@ import (
 	"text/template"
 )
 
-func replaceTemplate(yamlContent string, data map[string]interface{}) (string, error) {
+func ReplaceTemplate(yamlContent string, data map[string]interface{}) (string, error) {
 	tmpl, err := template.New("yamlTemplate").Parse(yamlContent)
 	if err != nil {
 		return "", err
