286203125cd44524f02496ce3edf4a6ed9210285
LronDC <lun.su@daocloud.io>
2023-12-08 18:28:27 +0800

    add some log

--- evaluation/in_tree_scoring.py
+++ evaluation/in_tree_scoring.py
@@ -49,25 +49,28 @@ for index, q in enumerate(questions, start=1):
             predictions.append(response.json()["output"])
 
             # 打印请求时间和进度
-            print(f"Question {index}/{total_questions} | Request Time: {end_time - start_time:.2f} seconds")
+            print(f"Question {index}/{total_questions} | Request Time: {end_time - start_time:.2f} seconds", flush=True)
             break
         except requests.RequestException as e:
-            print(f"Error during request: {e}")
+            print(f"Error during request: {e}", flush=True)
             retries += 1
             if retries < max_retries:
-                print(f"Retrying in {retry_interval} seconds...")
+                print(f"Retrying in {retry_interval} seconds...", flush=True)
                 time.sleep(retry_interval)
             else:
-                print(f"Max retries reached. Skipping question: {q}")
+                print(f"Max retries reached. Skipping question: {q}", flush=True)
 
 references = [sample['references'] for sample in samples]
 
-rouge = evaluate.load(".module/rouge")
+print("Start evaluating ROUGE metrics...", flush=True)
+rouge = evaluate.load("module/rouge")
 rouge_result = rouge.compute(predictions=predictions, references=references)
+print(f"ROUGE metrics evaluation finished: {rouge_result}", flush=True)
 
-bleu = evaluate.load(".module/bleu")
+print("Start evaluating BLEU metrics...", flush=True)
+bleu = evaluate.load("module/bleu")
 bleu_result = bleu.compute(predictions=predictions, references=references)
-
+print(f"BLEU metrics evaluation finished: {bleu_result}", flush=True)
 
 rouge1_weight=0.35
 rouge2_weight=0.4
@@ -91,4 +94,4 @@ bleu_result_str = {key: str(value) for key, value in bleu_result.items()}
 
 data = {"score": str(score), "metrics": ["ROUGE", "BLEU"], "details": {**rouge_result_str, **bleu_result_str}}
 result = requests.post(complete_notify_url, data=json.dumps(data), headers={"content-type": "application/json"})
-print(result.content)
+print(result.content, flush=True)
